6f764ca4856b5fc902d573439e9d52e584b3eb66
plwebse <peter.lindblom@plweb.se>
2021-03-11 20:45:23 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const querystring = require("querystring");
 const fetch = require('node-fetch');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
@@ -21,64 +20,16 @@ app.get("/", function (req, res) {
                 console.log("/ printing result from: " + req.query.url);
                 res.send(printHtml(json, req.query.url));
             }).catch(err => {
-                console.log("/ error: " + err);
-                console.log(err);
+                console.log("/ error: " + err);                
                 res.send(renderHtmlDocument('', req.query.url, err));
             });
     } else {
         console.log("/ ");
-        res.send(renderHtmlDocument('', '', ''));
+        res.send(renderHtmlDocument());
     }
 });
 
 app.listen(8000);
-
-function printHtml(json, requestedUrl) {
-
-    let tableRows = renderTableRow(tableHeaderColumns, "th");
-
-    json.forEach(element => {
-        const values = [element.url, element.msg];
-        cacheHeaderNames.forEach(cacheHeaderName => {
-            values.push(element.headers[cacheHeaderName] ? JSON.stringify(element.headers[cacheHeaderName]) : "");
-        });
-
-        tableRows += renderTableRow(values, "td");
-    });
-
-    return renderHtmlDocument(`<br><table cellpadding="3" width="100%">${tableRows}</table>`, requestedUrl, "");
-}
-
-function findLinksInHtml(html) {
-    const dom = new JSDOM(html);
-    let nodeList = dom.window.document.querySelectorAll("a");
-    let list = [];
-
-    nodeList.forEach(nodeItem => {
-        if ((nodeItem.href + "").startsWith("http")) {
-            list.push(nodeItem.href);
-        }
-    });
-
-    return list;
-}
-
-function getHeadersFromUrls(urls) {
-    return Promise.all(urls.map(url => {
-        return getHeadersFromUrl(url)
-    }));
-}
-
-function getHeadersFromUrl(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(res => {
-            resolve({ url, headers: res.headers.raw(), msg: '' });
-        }).catch(err => {
-            console.error(err);
-            resolve({ url, headers: {}, msg: err });
-        });
-    });
-}
 
 function getContentFromUrl(url) {
     return new Promise((resolve, reject) => {
@@ -90,84 +41,134 @@ function getContentFromUrl(url) {
     });
 }
 
+function findLinksInHtml(html) {
+    const dom = new JSDOM(html);
+    const nodeList = dom.window.document.querySelectorAll("a");
+    let list = [];
+
+    nodeList.forEach(nodeItem => {
+       if ((nodeItem.href + "").startsWith("http")) {
+           list.push(nodeItem.href);
+       }
+    });
+
+    return list;
+}
+
+function getHeadersFromUrls(urls) {
+    return Promise.all(urls.map(url => getHeadersFromUrl(url)));
+}
+
+function getHeadersFromUrl(url) {
+    return new Promise((resolve) => {
+        fetch(url).then(res => {
+            resolve({ url, headers: res.headers.raw(), msg: '' });
+        }).catch(err => {
+            console.error(err);
+            resolve({ url, headers: {}, msg: err });
+        });
+    });
+}
+
+
+function printHtml(json, requestedUrl) {
+
+    let tableRows = renderTableRow(tableHeaderColumns, "th");
+
+    json.forEach(element => {
+        const values = [element.url, element.msg];
+        
+        cacheHeaderNames.forEach(cacheHeaderName => {
+            values.push(element.headers[cacheHeaderName] ? JSON.stringify(element.headers[cacheHeaderName]) : "");
+        });
+
+        tableRows += renderTableRow(values, "td");
+    });
+
+    return renderHtmlDocument(tableRows, requestedUrl);
+}
+
+
+
+
+
 function renderTableRow(values, htmlTag) {
-    const str = values.map(value => { return "<" + htmlTag + ">" + value + "</" + htmlTag + ">" }).join("");
+    const str = values.map(value => "<" + htmlTag + ">" + value + "</" + htmlTag + ">").join("");
     return `<tr>${str}</tr>`
 }
 
 function renderHtmlDocument(bodyHtml, url, errorMsg) {
-
     const errorDiv = (errorMsg) ? "<div><b>" + errorMsg + "</b></div>" : "";
-    const bodyHtmlDiv = (bodyHtml) ? "<div>" + bodyHtml + "</div>" : "";
-
+    const bodyHtmlDiv = (bodyHtml) ? `<div><table cellpadding="3" width="100%">${bodyHtml}</table> </div>` : "";
     const baseHtml = `
 <html>
 <head>
-<title>Check that are links are cached</title>
-<style>
-    fieldset {
-        border: 1px solid #ccc;
-    }
+    <title>Check that are links are cached</title>
+    <style>
+        fieldset {
+            border: 1px solid #ccc;
+        }
 
-    fieldset>legend {
-        text-transform:uppercase;
-        font-weight: bold;
-        font-size:20pt;
-    } 
+        fieldset>legend {
+            text-transform:uppercase;
+            font-weight: bold;
+            font-size:20pt;
+        } 
 
-    table {
-        border: 1px solid #ccc;
-        border-collapse: collapse;
-        border-radius: 5px;
-        width:100%;        
-        
-    }
+        table {
+            border: 1px solid #ccc;
+            border-collapse: collapse;
+            border-radius: 5px;
+            width:100%;        
+            
+        }
 
-    table th, table td {
-        border: 1px solid #ccc;
-        padding:5px;
-        margin:5px;               
-    }
+        table th, table td {
+            border: 1px solid #ccc;
+            padding:5px;
+            margin:5px;               
+        }
 
-    table th {
-        background-color: #efefef;        
-        text-transform:uppercase;
-        text-align: left;
+        table tr:nth-child(odd) {
+            background-color: #efefef;  
+        }
 
-    }    
+        table th {
+            background-color: #efefef;        
+            text-transform:uppercase;
+            text-align: left;
+        }    
 
-    input {
-        width:100%;
-        padding:5px;
-        margin:5px;               
-    }
+        input {
+            width:100%;
+            padding:5px;
+            margin:5px;               
+        }
 
-    input[type='submit'] {
-        text-transform:uppercase;
-        font-weight: bold;               
-    }
+        input[type='submit'] {
+            text-transform:uppercase;
+            font-weight: bold;               
+        }
 
-    div#wrapper {
-        max-width:1200px;
-        margin:auto;
-        
-    }
+        div#wrapper {
+            max-width:1200px;
+            margin:auto;
+            
+        }
 
-    div#wrapper > div {        
-        margin:10px 0;
-        overflow:hidden;        
-        overflow-x:auto;
-    }
+        div#wrapper > div {        
+            margin:10px 0;
+            overflow:hidden;        
+            overflow-x:auto;
+        }
 
-    div#wrapper > div > b {
-        color:#FF0000;
-        margin: 20px 10px;
-    }        
+        div#wrapper > div > b {
+            color:#FF0000;
+            margin: 20px 10px;
+        }        
 
-
-
-</style>
-</head>
+    </style>
+    </head>
 <body>
 
 <div id="wrapper">
